6b897d575bfb862f963fd4d9e8306e966c372b70
RaymondWang987 <49090544+RaymondWang987@users.noreply.github.com>
2020-11-17 13:31:55 +0800

    Add files via upload

---
+++ seekforfood.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 09:23:35 2020
+
+@author: raymondwang
+"""
+
+# 0 西一1楼/1 西一二楼/2 西二2楼/3 东一一楼/4 东一二楼/5 东三/6 百景园1楼/7 百景园二楼/8 集贤楼/9 集锦园/
+import numpy as np
+print(np.random.randint(0, high=10))
+
+
